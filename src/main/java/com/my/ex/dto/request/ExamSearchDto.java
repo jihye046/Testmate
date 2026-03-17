46cd5bf798a7b2075f3af72ecc57b7da83d5a258
jihye046 <songg046@nate.com>
2026-03-17 15:44:50 +0900

    [FEAT] 시험지 검색 결과 무한 스크롤

--- src/main/java/com/my/ex/dto/request/ExamSearchDto.java
+++ src/main/java/com/my/ex/dto/request/ExamSearchDto.java
@@ -10,4 +10,13 @@ public class ExamSearchDto {
 	private String year;
 	private String round;
 	private int activeFolderId;
+	private int page;
+
+	// DB 조회용
+	private int startRow;
+	private int endRow;
+
+	public void setPage(int page) {
+		this.page = page == 0 ? 1 : page;
+	}
 }
